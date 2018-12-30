7229b3fee227c45c6adb107c024e1dc717938234
anindith123 <anindith123@ymail.com>
2018-12-30 03:10:24 -0500

    modified code to render iamges from s3 bucket, changes to reduce load

--- src/components/Aboutskills.js
+++ src/components/Aboutskills.js
@@ -6,10 +6,10 @@ class Aboutskills extends Component {
     render() {
         return <section >
         <div className = "centerAlign">
-        <h6><b>Master of Science-University of Cincinnati</b> GPA-3.6</h6>
+        <h6><strong>Master of Science-University of Cincinnati</strong> GPA-3.6</h6>
             <h6>Information Technology  Aug'17–Dec'18</h6>
              <p style={{fontsize : '8px'}}>Research Based Learning, Human Computer Interaction,IT Infrastructure, Intelligent Data Analysis, Information Retrieval, Database Theory</p>
-            <h6><b>Bachelor of Technology -VIgnan Institute of Technology</b> GPA-3.7</h6>
+            <h6><strong>Bachelor of Technology -VIgnan Institute of Technology</strong> GPA-3.7</h6>
             <h6>Computer Sciences  Aug'13–May'17</h6>
             <p>Data Structures, Information Security, Web Technologies,OOP, Software Development Methodologies, Database.
                 Certificate- Python, HTML, CSS, Cloud computing
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,35 +2,16 @@ import React, { Component } from 'react';
 import '../styles/css/bootstrap.min.css'
 import '../styles/css/new_styles.css'
 import { Modal } from 'react-bootstrap';
-import PhotoBooth from '../styles/css/PhotoBooth.jpg';
-import RedditClone from '../styles/css/RedditClone.jpg';
-import ShoppingCart from '../styles/css/ShoppingCart.jpg';
-import StoresRESTAPI from '../styles/css/StoresRESTAPI.jpg';
-import BurgerOrder from '../styles/css/BurgerOrder.jpg';
-import CollegeManagementSystem from '../styles/css/CollegeManagementSystem.jpg';
-import WeatherData from '../styles/css/weatherData.jpg';
-
-
 
 class Project extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-        show : false,
-        projimg : {
-            'PhotoBooth' : PhotoBooth,
-            'RedditClone' : RedditClone,
-            'ShoppingCart' : ShoppingCart,
-            'StoresRESTAPI' : StoresRESTAPI,
-            'BurgerOrder' : BurgerOrder,
-            'CollegeManagementSystem' : CollegeManagementSystem,
-            'WeatherData': WeatherData
-
-        }
+            show: false
         };
-            this.handleShow = this.handleShow.bind(this);
-            this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
     handleClose() {
         this.setState({ show: false });
@@ -42,10 +23,9 @@ class Project extends Component {
 
     render() {
         let data = this.props.projdat
-        let projimg = this.state.projimg[data['title']]
         return <div>
             <div className="card col-sm-12 col-md-12 centerAlign" style={{ width: '100%' }}>
-                <img className="card-img-top projectImg" src={projimg} alt="project view"/>
+                <img className="card-img-top projectImg" src={data['image_link']} alt="project view" />
                 <div className="card-body centerAlign">
                     <h4 className="card-title">{data ? data['title'] : "Loading..."}</h4>
                     <p className="card-text">{data ? data['short_desc'] : "Loading..."}</p>
@@ -63,7 +43,7 @@ class Project extends Component {
                     <b>Tools:</b><p>{data ? data['tools'] : "Loading..."}</p>
                 </Modal.Body>
                 <Modal.Footer>
-                    <a type= "button" href={data['git_link']} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Visit</a>
+                    <a type="button" href={data['git_link']} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Visit</a>
                     <button className="btn btn-primary" onClick={this.handleClose}>Close</button>
                 </Modal.Footer>
             </Modal>
